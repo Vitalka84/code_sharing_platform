c89d05e3f92a4610a7b6c12a83c51b2eaa430ebb
vetal <pasternakv@edi.su>
2021-04-17 23:13:43 +0300

    up

--- src/platform/HtmlBuilder.java
+++ src/platform/HtmlBuilder.java
@@ -7,11 +7,15 @@ public class HtmlBuilder {
     String title;
     List<String> bodyContent;
     List<String> jsFunctions;
+    List<String> linksList;
+    List<String> scriptTagsList;
     List<DivBlockBuilder> divBlocks;
 
     public HtmlBuilder() {
         this.bodyContent = new ArrayList<>();
         this.jsFunctions = new ArrayList<>();
+        this.linksList = new ArrayList<>();
+        this.scriptTagsList = new ArrayList<>();
     }
 
     public void setTitle(String title) {
@@ -20,6 +24,37 @@ public class HtmlBuilder {
 
     public void setDivBlocks(List<DivBlockBuilder> divBlocks) {
         this.divBlocks = divBlocks;
+    }
+
+    public void setLink(String href, String rel, String type) {
+        if (href != null && !"".equals(href)) {
+            StringBuilder linkTag = new StringBuilder();
+            linkTag.append("<link href=\"" + href + "\"");
+            if (rel != null && !"".equals(rel)) {
+                linkTag.append(" rel=\"" + rel + "\"");
+            }
+            if (type != null && !"".equals(type)) {
+                linkTag.append(" rel=\"" + type + "\"");
+            }
+            linkTag.append(">");
+            linksList.add(linkTag.toString());
+        }
+    }
+
+    public void setScriptTag(String data, String src) {
+        if ((data != null && !"".equals(data)) || (src != null && !"".equals(src))) {
+            StringBuilder scriptTag = new StringBuilder();
+            scriptTag.append("<script");
+            if (src != null && !"".equals(src)) {
+                scriptTag.append(" src=\"" + src + "\"");
+            }
+            scriptTag.append(">");
+            if (data != null && !"".equals(data)) {
+                scriptTag.append(data);
+            }
+            scriptTag.append("</script>");
+            scriptTagsList.add(scriptTag.toString());
+        }
     }
 
     public void setPreWrapper(String data, String id) {
@@ -61,6 +96,17 @@ public class HtmlBuilder {
         res.append("<title>");
         res.append(this.title == null ? "Code" : this.title);
         res.append("</title>");
+        if (linksList != null) {
+            for (String link : linksList) {
+                res.append(link);
+            }
+        }
+        if (scriptTagsList != null) {
+            for (String scriptTag : scriptTagsList) {
+                res.append(scriptTag);
+            }
+        }
+        res.append("</head>");
         res.append("<body>");
         if (divBlocks != null) {
             for (DivBlockBuilder divBlock : divBlocks) {
@@ -71,7 +117,6 @@ public class HtmlBuilder {
             res.append(body);
         }
         res.append("</body>");
-        res.append("</head>");
         res.append("</html>");
         if (!jsFunctions.isEmpty()) {
             res.append("<script type=\"text/javascript\">");
